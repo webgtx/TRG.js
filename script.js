8d4b09ba610265b85e4e58a632c23631c454771f
webgtx <dxv1d@tuta.io>
2021-02-25 14:26:18 +0200

    First patch

---
+++ script.js
@@ -0,0 +1,82 @@
+
+const button = document.querySelector('.button-play');
+
+button.addEventListener('click', () => {
+  creatingHero();
+  alert(`Now your hero was created
+      Write /help for more info`);
+  interfaceHero();
+});
+
+function creatingHero() {
+  let heroName = prompt('Choose name for your hero');
+  if (!heroName == '' || !heroName == 0) {
+    hero.name = heroName;
+  } else {
+    while (heroName == '' || heroName == 0) {
+      alert('Pls write your name correct');
+      heroName = prompt('Choose name for your hero');
+    }
+  }
+  alert(`You can choose 2 classes for your hero
+        1) Warrior
+        2) Archer`)
+  let heroClass = prompt('Choose your class')
+  if (heroClass == 'Warrior') {
+    hero.class = heroClass;
+    hero.weapon = 'Stone sword'
+    hero.items.push('Apple')
+  } else if (heroClass == 'Archer') {
+    hero.class = heroClass;
+    hero.weapon = 'Wooden bow'
+    hero.items.push('Apple')
+  } else {
+    alert('Abort')
+  }
+
+};
+
+function interfaceHero() {
+  let commandLine;
+  do {
+    commandLine = prompt('Write your command with /')
+    if (commandLine == '/exit') {
+      break;
+    } else if (commandLine == '/help') {
+      alert(`All commands
+            /stats - Info about hero
+            /items - Your items
+            /quest - Your quests from npc
+            /exit - Delete this session
+            /walk - Explore dungeon`);
+    } else if (commandLine == '/stats') {
+      alert(`Your stats:
+        Name: ${hero.name}
+        Class: ${hero.class}
+        Weapon: ${hero.weapon}`);
+    } else if (commandLine == '/walk') {
+      alert('You strolling throught the dark')
+      const findingEnemy = randomModule(1, 100)
+      if (findingEnemy > 70) {
+        alert(`You saw a lot of goblins`);
+        fightModule();
+      } else {
+        alert('Nothing here, maybe later i find them.')
+      }
+    }
+  } while (true);
+};
+
+function randomModule(min, max) {
+  return Math.floor(Math.random() * (min + max + 1) + min);
+}
+function fightModule() {
+
+}
+
+const hero = {
+  name: '',
+  class: '',
+  weapon: '',
+  items: [],
+}
